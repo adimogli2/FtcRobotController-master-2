0fa5107cea6ae006bb23c6e9b26714aca3b338df
adimogli2 <adimogli2@gmail.com>
2023-06-17 19:04:18 -0700

    Created signalclassifierupdated

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SignalClassifierUpdated.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SignalClassifierUpdated.java
@@ -2,7 +2,6 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvCameraRotation;
@@ -13,20 +12,12 @@ import org.opencv.core.Mat;
 import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 
-@TeleOp(name = "Traffic Signal Classifier")
-public class TrafficSignalClassifier extends LinearOpMode {
+@TeleOp(name = "Signal Classifier Updated")
+public class SignalClassifierUpdated extends LinearOpMode {
     private OpenCvCamera camera;
-    private DcMotor FL0;
-    private DcMotor BL1;
-    private DcMotor BR2;
-    private DcMotor FR3;
 
     @Override
     public void runOpMode() {
-        FL0 = hardwareMap.get(DcMotor.class, "Front_Left");
-        BL1 = hardwareMap.get(DcMotor.class, "Back_Left");
-        BR2 = hardwareMap.get(DcMotor.class, "Back_Right");
-        FR3 = hardwareMap.get(DcMotor.class, "Front_Right");
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         camera = OpenCvCameraFactory.getInstance().createInternalCamera(OpenCvInternalCamera.CameraDirection.BACK, cameraMonitorViewId);
         camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
@@ -37,7 +28,7 @@ public class TrafficSignalClassifier extends LinearOpMode {
 
             @Override
             public void onError(int errorCode) {
-
+                
             }
         });
         camera.setPipeline(new SignalPipeline());
@@ -54,42 +45,15 @@ public class TrafficSignalClassifier extends LinearOpMode {
         public Mat processFrame(Mat input) {
             Mat hsv = new Mat();
             Imgproc.cvtColor(input, hsv, Imgproc.COLOR_RGB2HSV);
-            Mat maskRed = new Mat();
             Mat maskGreen = new Mat();
-            Mat maskYellow = new Mat();
-            Core.inRange(hsv, new Scalar(0, 70, 50), new Scalar(10, 255, 255), maskRed);
             Core.inRange(hsv, new Scalar(38, 70, 50), new Scalar(75, 255, 255), maskGreen);
-            Core.inRange(hsv, new Scalar(20, 100, 100), new Scalar(30, 255, 255), maskYellow);
-            int redCount = Core.countNonZero(maskRed);
             int greenCount = Core.countNonZero(maskGreen);
-            int yellowCount = Core.countNonZero(maskYellow);
-            if (redCount > greenCount && redCount > yellowCount) {
-                // red signal
-                telemetry.addData("Signal", "Red");
-                telemetry.update();
-                FL0.setPower(0);
-                BL1.setPower(0);
-                BR2.setPower(0);
-                FR3.setPower(0);
-            } else if (greenCount > redCount && greenCount > yellowCount) {
+            if (greenCount > 0) {
                 // green signal
                 telemetry.addData("Signal", "Green");
                 telemetry.update();
-                FL0.setPower(0.3);
-                BL1.setPower(0.3);
-                BR2.setPower(0.3);
-                FR3.setPower(0.3);
-            } else if (yellowCount > redCount && yellowCount > greenCount) {
-                // yellow signal
-                telemetry.addData("Signal", "Yellow");
-                telemetry.update();
-                FL0.setPower(0.1);
-                BL1.setPower(0.1);
-                BR2.setPower(0.1);
-                FR3.setPower(0.1);
             }
             return input;
         }
     }
 }
-
