fe8b68eb3640555830b8bd1e89545feaef0047a1
adimogli2 <55467101+adimogli2@users.noreply.github.com>
2023-08-20 12:37:32 -0700

    test.java contains a working version of code that saves image frames

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test.java
@@ -1,73 +1,91 @@
 package org.firstinspires.ftc.teamcode;
 
+import static android.os.Environment.getExternalStorageDirectory;
+
+import android.os.Environment;
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.HardwareMap;
-import org.firstinspires.ftc.robotcore.external.ClassFactory;
-import org.firstinspires.ftc.robotcore.external.hardware.camera.CameraName;
+
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
 import org.opencv.videoio.VideoCapture;
 import org.opencv.videoio.VideoWriter;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvCameraRotation;
 import org.openftc.easyopencv.OpenCvInternalCamera;
+import org.openftc.easyopencv.OpenCvPipeline;
 
-@TeleOp(name = "test", group = "Iterative Opmode")
+
+import java.io.File;
+
+@TeleOp(name="test", group="Iterative Opmode")
 public class test extends LinearOpMode {
-    private DcMotor motorFL0;
-    private DcMotor motorBL1;
-    private DcMotor motorBR2;
-    private DcMotor motorFR3;
+    private DcMotor FL0;
+    private DcMotor BL1;
+    private DcMotor BR2;
+    private DcMotor FR3;
 
-    private CameraName cameraName;
-    private VideoCapture videoCapture;
+    private OpenCvCamera camera;
+
+    private VideoCapture videoCapture = new VideoCapture();
     private VideoWriter videoWriter;
 
-    private static final int CAMERA_FRAME_WIDTH = 640;
+    private static final int  CAMERA_FRAME_WIDTH = 640;
     private static final int CAMERA_FRAME_HEIGHT = 480;
     private static final int CAMERA_FRAME_RATE = 30;
-    private static final String VIDEO_FILE_EXTENSION = ".avi";
+    private static final String VIDEO_FILE_EXTENSION = ".jpg";
 
     @Override
     public void runOpMode() {
-        initializeHardware();
-        initializeVideoRecording();
+        try {
+            initializeHardware();
+            initializeVideoRecording();
+            waitForStart();
 
-        waitForStart();
-
-        if (opModeIsActive()) {
             while (opModeIsActive()) {
-                double motorFL0Power = motorFL0.getPower();
-                double motorBL1Power = motorBL1.getPower();
-                double motorBR2Power = motorBR2.getPower();
-                double motorFR3Power = motorFR3.getPower();
+                double tgp_x = gamepad1.left_stick_x;
+                double tgp_y = gamepad1.left_stick_y * -1;
+                double rot_x = gamepad1.right_stick_x;
+                double rot_y = gamepad1.right_stick_y;
+                boolean mac1 = gamepad1.dpad_right;
 
-                // Update motor powers and write frame to the video file
-                updateMotorPowers(motorFL0Power, motorBL1Power, motorBR2Power, motorFR3Power);
+                // Update motor powers
+                updateMotorPowers(tgp_x, tgp_y, rot_x, rot_y, mac1);
+
+                // Write frame to the video file
                 writeFrameToVideo();
-
-                // Update telemetry
-                telemetry.addData("Motor FL Power", motorFL0Power);
-                telemetry.addData("Motor BL Power", motorBL1Power);
-                telemetry.addData("Motor BR Power", motorBR2Power);
-                telemetry.addData("Motor FR Power", motorFR3Power);
-                telemetry.update();
             }
+        } catch (Exception e) {
+            telemetry.addData("Error", e.getMessage());
+            telemetry.update();
+        } finally {
+            //stopVideoRecording();
         }
-
-        stopVideoRecording();
     }
 
     private void initializeHardware() {
-        motorFL0 = hardwareMap.get(DcMotor.class, "Front_Left");
-        motorBL1 = hardwareMap.get(DcMotor.class, "Back_Left");
-        motorBR2 = hardwareMap.get(DcMotor.class, "Back_Right");
-        motorFR3 = hardwareMap.get(DcMotor.class, "Front_Right");
+        FL0 = hardwareMap.get(DcMotor.class, "Front_Left");
+        BL1 = hardwareMap.get(DcMotor.class, "Back_Left");
+        BR2 = hardwareMap.get(DcMotor.class, "Back_Right");
+        FR3 = hardwareMap.get(DcMotor.class, "Front_Right");
+        // Initialize other hardware components if needed
+    }
+
+
+    private void initializeVideoRecording() {
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
-        OpenCvInternalCamera camera = OpenCvCameraFactory.getInstance().createInternalCamera(OpenCvInternalCamera.CameraDirection.BACK, cameraMonitorViewId);
+        String fileName = String.format("%f_%f_%f_%f%s", FL0.getPower(), BL1.getPower(), BR2.getPower(), FR3.getPower(), VIDEO_FILE_EXTENSION);
+        File videoFile = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM), fileName);
+        //to save image sequence, use a proper filename; set fourcc=0 or fps=0
+        videoWriter = new VideoWriter(videoFile.getAbsolutePath(), 0, 0, new Size(240, 320), true);
+        camera = OpenCvCameraFactory.getInstance().createInternalCamera(OpenCvInternalCamera.CameraDirection.BACK, cameraMonitorViewId);
+
         camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
             @Override
             public void onOpened() {
@@ -79,43 +97,74 @@ public class test extends LinearOpMode {
 
             }
         });
-        // Configure and initialize other hardware components
+        camera.setPipeline(new SignalPipeline());
     }
 
-    private void initializeVideoRecording() {
-        // Initialize OpenCV
-        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-
-        // Initialize the video capture
-        videoCapture = new VideoCapture();
-        videoCapture.open(0); // 0 represents the default internal camera
-
-        // Configure video capture properties
-        videoCapture.set(3, CAMERA_FRAME_WIDTH);
-        videoCapture.set(4, CAMERA_FRAME_HEIGHT);
-        videoCapture.set(5, CAMERA_FRAME_RATE);
-
-        // Initialize the video writer
-        String videoFilename = String.format("%d_%d_%d_%d%s", motorFL0.getPower(), motorBL1.getPower(), motorBR2.getPower(), motorFR3.getPower(), VIDEO_FILE_EXTENSION);
-        videoWriter = new VideoWriter(videoFilename, VideoWriter.fourcc('M', 'J', 'P', 'G'), CAMERA_FRAME_RATE, new org.opencv.core.Size(CAMERA_FRAME_WIDTH, CAMERA_FRAME_HEIGHT));
-    }
-
-    private void updateMotorPowers(double motorFLPower, double motorBLPower, double motorBRPower, double motorFRPower) {
-        motorFL0.setPower(motorFLPower);
-        motorBL1.setPower(motorBLPower);
-        motorBR2.setPower(motorBRPower);
-        motorFR3.setPower(motorFRPower);
+    private void updateMotorPowers(double tgp_x, double tgp_y, double rot_x, double rot_y, boolean mac1) {
+        // Update motor powers based on joystick input and other conditions
+        // Modify the logic as per your requirements
+        FL0.setPower(tgp_y);
+        BL1.setPower(tgp_y);
+        BR2.setPower(tgp_y);
+        FR3.setPower(tgp_y);
     }
 
     private void writeFrameToVideo() {
         Mat frame = new Mat();
+        Mat cv_frame = new Mat();
         if (videoCapture.read(frame)) {
-            videoWriter.write(frame);
+            Imgproc.cvtColor(cv_frame, frame, Imgproc.COLOR_BGR2HSV);
+            videoWriter.write(cv_frame);
         }
     }
 
     private void stopVideoRecording() {
         videoCapture.release();
         videoWriter.release();
+    }
+
+    class SignalPipeline extends OpenCvPipeline {
+        @Override
+        public Mat processFrame(Mat input) {
+            Mat hsv = new Mat();
+            //Imgproc.cvtColor(input, hsv, Imgproc.COLOR_RGB2HSV);
+            Imgproc.cvtColor(input, hsv, Imgproc.COLOR_BGR2HSV);
+            Mat maskRed = new Mat();
+            Mat maskGreen = new Mat();
+            Mat maskYellow = new Mat();
+            Core.inRange(hsv, new Scalar(0, 70, 50), new Scalar(10, 255, 255), maskRed);
+            Core.inRange(hsv, new Scalar(38, 70, 50), new Scalar(75, 255, 255), maskGreen);
+            Core.inRange(hsv, new Scalar(20, 100, 100), new Scalar(30, 255, 255), maskYellow);
+            int redCount = Core.countNonZero(maskRed);
+            int greenCount = Core.countNonZero(maskGreen);
+            int yellowCount = Core.countNonZero(maskYellow);
+            if (redCount > greenCount && redCount > yellowCount) {
+                // red signal
+                telemetry.addData("Signal", "Red");
+                telemetry.update();
+                FL0.setPower(0);
+                BL1.setPower(0);
+                BR2.setPower(0);
+                FR3.setPower(0);
+            } else if (greenCount > redCount && greenCount > yellowCount) {
+                // green signal
+                telemetry.addData("Signal", "Green");
+                telemetry.update();
+                FL0.setPower(0.3);
+                BL1.setPower(0.3);
+                BR2.setPower(0.3);
+                FR3.setPower(0.3);
+            } else if (yellowCount > redCount && yellowCount > greenCount) {
+                // yellow signal
+                telemetry.addData("Signal", "Yellow");
+                telemetry.update();
+                FL0.setPower(0.1);
+                BL1.setPower(0.1);
+                BR2.setPower(0.1);
+                FR3.setPower(0.1);
+            }
+            videoWriter.write(input);
+            return input;
+        }
     }
 }
